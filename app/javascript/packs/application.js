e0b3e72a338d10f81afd1e28d1df80a1c9e619f9
tiberius-ls <ogbonnaemeka543@gmail.com>
2021-08-31 13:58:39 +0100

    added sortablejs and stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,21 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import Sortable from "sortablejs";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 import "../stylesheets/application"
+
+import "controllers"
+
+
+document.addEventListener("turbolinks:load", () => {
+    var el = document.getElementById("tasks-list");
+    var sortable = Sortable.create(el, {
+        Animation: 150,
+        ghostClass: 'bg-light'
+    });
+})
